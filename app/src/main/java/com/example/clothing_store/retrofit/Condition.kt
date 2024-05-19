157164bfd674e7130d387c083d017c9f40ba8ba7
Zolomed <sasha.zolotarev10@gmail.com>
2024-05-20 02:18:55 +0300

    Перезолив

--- app/src/main/java/com/example/clothing_store/retrofit/Condition.kt
+++
@@ -1,7 +0,0 @@
-package com.example.clothing_store.retrofit
-
-data class Condition(
-    val code: Int,
-    val icon: String,
-    val text: String
-)
--- app/src/main/java/com/example/clothing_store/retrofit/Weather.kt
+++
@@ -1,6 +0,0 @@
-package com.example.clothing_store.retrofit
-
-data class Weather(
-    val current: Current,
-    val location: Location,
-)
