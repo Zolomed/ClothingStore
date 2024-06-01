fa962da9959bb1c53f278e59befa18c481d25171
Zolomed <sasha.zolotarev10@gmail.com>
2024-06-02 01:15:16 +0300

    Добавление local items

--- app/src/main/java/com/example/clothing_store/activity/ItemsActivity.kt
+++ app/src/main/java/com/example/clothing_store/activity/ItemsActivity.kt
@@ -7,14 +7,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.clothing_store.R
+import com.example.clothing_store.adapter.ItemAdapter
+import com.example.clothing_store.model.Item
 
 class ItemsActivity : AppCompatActivity() {
 
     private lateinit var toSearchButton: ImageButton
+    private lateinit var itemRV: RecyclerView
     private lateinit var basketButton: ImageButton
     private lateinit var favouritesButton: ImageButton
     private lateinit var accountButton: ImageButton
+
+    private val adapter = ItemAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,10 +39,16 @@ class ItemsActivity : AppCompatActivity() {
         favouritesButton = findViewById(R.id.favouritesButton)
         accountButton = findViewById(R.id.accountButton)
 
+        itemRV = findViewById(R.id.itemRV)
+        itemRV.layoutManager = LinearLayoutManager(this)
+        itemRV.adapter = adapter
+
         toSearchButton.setOnClickListener {
             val intent = Intent(this, SearchActivity::class.java)
             startActivity(intent)
         }
+
+
 
         basketButton.setOnClickListener {
             val intent = Intent(this, BasketActivity::class.java)
---
+++ app/src/main/java/com/example/clothing_store/model/Item.kt
@@ -0,0 +1,7 @@
+package com.example.clothing_store.model
+
+data class Item(
+    val imageId: Int,
+    val name: String,
+    val price: Int
+)
